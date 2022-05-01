9da5a1e76f895e775cf79152fbb16c61fad656ab
Christian Schulz <55575593+cschulzsuper@users.noreply.github.com>
2022-05-01 09:59:28 +0200

    Local localization system (#19)

---
+++ src/application/Transport.Contract/Localization/Exceptions/TranslationNotFoundException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Super.Paula.Application.Auth.Exceptions
+{
+    public class TranslationNotFoundException : Exception
+    {
+        public TranslationNotFoundException(FormattableString message)
+            : base(message.ToString())
+        {
+
+        }
+
+        public TranslationNotFoundException(FormattableString message, Exception innerException)
+            : base(message.ToString(), innerException)
+        {
+
+        }
+    }
+}
---
+++ src/blazor/Client/Localization/TranslationHandlerFactory.cs
@@ -0,0 +1,25 @@
+﻿using Super.Paula.Application.Auth.Exceptions;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace Super.Paula.Client.Localization
+{
+    public class TranslationHandlerFactory
+    {
+
+        public static ITranslationHandler Create()
+        {
+            var currentCulture = CultureInfo.CurrentCulture.Name;
+
+            return currentCulture switch
+            {
+                "de" or "de-AT" or "de-CH" or "de-DE" or "de-LI" or "de-LU"
+                    => new LocalGermanTranslationHandler(),
+
+                _ => new LocalFallbackTranslationHandler(),
+            };
+        }
+    }
+}
